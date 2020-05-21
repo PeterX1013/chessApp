395fda19bee4d233af06b025cde036194a967792
crwandle <cwandle@gmail.com>
2020-05-21 14:44:12 -0700

    getignore

--- window.py
+++ window.py
@@ -1,14 +1,22 @@
 import pygame, sys
-pygame.init()
-window = pygame.display.set_mode((800, 650))
-pygame.display.set_caption("chessApp")
-window.fill((128, 0, 0))
 
-playGame = True
 
-# Loop for creating and exiting the window
-while playGame:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            playGame = False
-    pygame.display.flip()
+class chessWindow:
+    pygame.init()
+
+    def createwindow(self):
+        window = pygame.display.set_mode((800, 650))
+        pygame.display.set_caption("chessApp")
+        window.fill((128, 0, 0))
+
+        play_game = True
+
+        # Loop for creating and exiting the window
+        while play_game:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    play_game = False
+            pygame.display.flip()
+
+    if __name__ == "__main__":
+        createwindow()
