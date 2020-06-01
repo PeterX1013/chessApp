0bf13045bb0dcd5a4176def847f2e429e9793504
Mars-98 <36772883+Mars-98@users.noreply.github.com>
2020-06-01 17:26:07 -0500

    Parent piece (#19)
    * gitignore
    
    * test
    
    * test
    
    * #76ebhv worked on design for api
    
    * #76jre6
    
    * #76jre6
    
    * #76jre6
    
    * #76jre6
    
    * #76jre6
    
    * #76jre6
    
    * #76jre6
    
    * #76jre6

--- chess-api/parentPiece/piece.py
+++ chess-api/parentPiece/piece.py
@@ -4,12 +4,11 @@ from abc import ABC, abstractmethod
 class Piece(ABC):
     totalPieces = 16
 
-    def __init__(self, piece_move_cnt, piece_points, piece_color, piece_curr_loc, piece_first_move):
+    def __init__(self, piece_move_cnt, piece_points, piece_color, piece_curr_loc):
         self.move_count = piece_move_cnt
         self.points = piece_points
         self.color = piece_color
         self.curr_loc = piece_curr_loc
-        self.first_move = piece_first_move
 
     def __del__(self, piece_move_cnt, piece_points, piece_color, piece_curr_loc):
         del piece_move_cnt
@@ -29,4 +28,4 @@ class Piece(ABC):
             enemy_piece.curr_loc = None
             self.totalPieces -= 1
 
-        return  self.totalPieces
+        return self.totalPieces
