8541e287ec36d71b47e139ab1877013d112f18fe
coughenberg <48074251+coughenberg@users.noreply.github.com>
2020-06-02 19:28:23 -0500

    CU-7pdmvc added self to ChessWindow methods (#20)

--- chess-ui/main.py
+++ chess-ui/main.py
@@ -3,4 +3,4 @@ from window import ChessWindow
 # Take name window here and grab the values
 
 # Then thise createWindow() remains, and uses the player names from the form to place in the window
-ChessWindow.createWindow()
+ChessWindow().createWindow()
--- chess-ui/window.py
+++ chess-ui/window.py
@@ -8,7 +8,7 @@ class ChessWindow:
 
     # function for creating window and calling other functions (e.i. scoreboard) inside the window
     # components are made in order, window must be first then scoreboard, etc.
-    def createWindow():
+    def createWindow(self):
         window = pygame.display.set_mode((600, 600))
         pygame.display.set_caption("chessApp")
         window.fill((128, 0, 0))
@@ -31,8 +31,8 @@ class ChessWindow:
                     pos = pygame.mouse.get_pos()
 
                     # get a list of all sprites that are under the mouse cursor
-                    clicked_tile = ChessWindow.getTileSprite(pos, tiles)
-                    clicked_piece = ChessWindow.getPieceSprite(pos, pieces)
+                    clicked_tile = self.getTileSprite(pos, tiles)
+                    clicked_piece = self.getPieceSprite(pos, pieces)
                     
                     if clicked_tile and clicked_piece:
                         column, row = clicked_tile.parent_link
@@ -54,12 +54,12 @@ class ChessWindow:
 
             pygame.display.flip()
 
-    def getTileSprite(position, tiles):
+    def getTileSprite(self, position, tiles):
         for tile in tiles:
             if tile.rectangle.collidepoint(position):
                 return tile
     
-    def getPieceSprite(position, pieces):
+    def getPieceSprite(self, position, pieces):
         for piece in pieces:
             if piece.rectangle_placement.collidepoint(position):
                 return piece
